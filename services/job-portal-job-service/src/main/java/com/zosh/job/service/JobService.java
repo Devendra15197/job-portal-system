46adfab09fec7248dcd6c10166061c52ce5521b2
devendrapawar <devendrapawar666@gmail.com>
2026-07-30 19:35:17 +0530

    Repository and service interface added

---
+++ services/job-portal-job-service/src/main/java/com/zosh/job/service/JobService.java
@@ -0,0 +1,28 @@
+package com.zosh.job.service;
+
+import com.zosh.job.dto.JobRequest;
+import com.zosh.job.dto.JobResponse;
+import com.zosh.job.payload.JobSearchRequest;
+
+import java.util.List;
+
+public interface JobService {
+
+    JobResponse createJob(Long employerId, JobRequest jobRequest);
+
+    JobResponse updateJob(Long jobId, Long employerId, JobRequest jobRequest);
+
+    JobResponse getJobById(Long id) throws Exception;
+
+    List<JobResponse> getJobs(JobSearchRequest request);
+
+    List<JobResponse> getJobsByCompany(Long companyId);
+
+    JobResponse publishJob(Long jobId, Long employerId);
+
+    JobResponse closeJob(Long jobId, Long employerId);
+
+    JobResponse deleteJob(Long jobId, Long employerId);
+
+    List<JobResponse> getAllJobsAdmin();
+}
