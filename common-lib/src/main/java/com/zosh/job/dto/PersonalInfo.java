bb3bed326a28831a148f9b78af17690105366456
devendrapawar <devendrapawar666@gmail.com>
2026-08-04 17:23:42 +0530

    Embeddable class and Enums created

---
+++ common-lib/src/main/java/com/zosh/job/dto/PersonalInfo.java
@@ -0,0 +1,31 @@
+package com.zosh.job.dto;
+
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PersonalInfo {
+
+    private String firstName;
+    private String lastName;
+
+    private String headline;
+
+    private String email;
+
+    private String phone;
+
+    private String city;
+    private String country;
+
+    private String linkedinUrl;
+    private String githubUrl;
+    private String portfolioUrl;
+    private String websiteUrl;
+
+
+}
