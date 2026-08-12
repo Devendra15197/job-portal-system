dd046d4d1f86b7346628b0b2ab4811cf31540c25
devendrapawar <devendrapawar666@gmail.com>
2026-08-12 23:34:22 +0530

    Created Project Entity

---
+++ services/job-portal-resume-service/src/main/java/com/zosh/job/modal/Project.java
@@ -0,0 +1,59 @@
+package com.zosh.job.modal;
+
+import com.zosh.job.entity.Resume;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "projects")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Resume resume;
+
+    private String description;
+
+    @Column(nullable = false)
+    private String title;
+
+    @ElementCollection
+    private List<String> technologies;
+
+    private String projectUrl;
+
+    private String sourceCodeUrl;
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Boolean isOngoing = false;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Integer displayOrder= 0;
+
+    @Column(nullable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
