ec423f3150345b5fd83680ef30ee08dd2b480f85
devendrapawar <devendrapawar666@gmail.com>
2026-08-12 23:34:43 +0530

    Created Service Interface

---
+++ services/job-portal-resume-service/src/main/java/com/zosh/job/service/ProjectService.java
@@ -0,0 +1,17 @@
+package com.zosh.job.service;
+
+import com.zosh.job.dto.ProjectResponse;
+import com.zosh.job.payload.AddProjectRequest;
+
+import java.util.List;
+
+public interface ProjectService {
+
+    ProjectResponse addProject(Long resumeId, Long candidateId, AddProjectRequest addProjectRequest) throws Exception;
+
+    List<ProjectResponse> getAllProjects(Long resumeId);
+
+    ProjectResponse updateProject(Long projectId, Long resumeId, Long candidateId, AddProjectRequest addProjectRequest) throws Exception;
+
+    void deleteProject(Long projectId, Long resumeId, Long candidateId) throws Exception;
+}
