ab1cba9531bc7536e9c8ea2a97fc96e7eebad1bb
devendrapawar <devendrapawar666@gmail.com>
2026-08-15 22:54:33 +0530

    Created Application Service as a skeleton and tested with homecontroller

---
+++ services/job-portal-application-service/src/main/java/com/zosh/job/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.zosh.job.controller;
+
+
+import com.zosh.job.dto.ApiResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/home")
+public class HomeController {
+
+    @RequestMapping("/welcome")
+    public String welcome() {
+
+        return "Welcome to the APPLICATION Portal Company Service!" + new ApiResponse("New API Resoinse", true);
+    }
+}
+
