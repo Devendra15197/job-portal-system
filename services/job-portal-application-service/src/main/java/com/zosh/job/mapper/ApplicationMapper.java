dbd090a1a6ce99e7938f3b93b5b518986877ea8e
devendrapawar <devendrapawar666@gmail.com>
2026-08-17 22:20:44 +0530

    Application Mapper class created

--- services/job-portal-application-service/src/main/java/com/zosh/job/mapper/ApplicationMapper.java
+++ services/job-portal-application-service/src/main/java/com/zosh/job/mapper/ApplicationMapper.java
@@ -2,6 +2,8 @@ package com.zosh.job.mapper;
 
 import com.zosh.job.dto.ApplicationResponse;
 import com.zosh.job.dto.JobResponse;
+import com.zosh.job.dto.response.CompanyResponse;
+import com.zosh.job.dto.response.UserResponse;
 import com.zosh.job.modal.Application;
 import com.zosh.job.payload.CreateApplicationRequest;
 
@@ -23,9 +25,23 @@ public class ApplicationMapper {
 
     }
 
-    public static ApplicationResponse toResponse(Application application) {
+    public static ApplicationResponse toResponse(Application application, JobResponse job, CompanyResponse company, UserResponse candidate) {
         return ApplicationResponse.builder()
                 .id(application.getId())
+                .candidate(candidate)
+                .employerId(application.getEmployerId())
+                .job(job)
+                .company(company)
+                .status(application.getStatus())
+                .resumeId(application.getResumeId())
+                .coverLetter(application.getCoverLetter())
+                .expectedSalary(application.getExpectedSalary())
+                .availableFrom(application.getAvailableFrom())
+                .isStarred(application.getIsStarred())
+                .withdrawnAt(application.getWithdrawnAt())
+                .appliedAt(application.getAppliedAt())
+                .updatedAt(application.getUpdatedAt())
+
                 .build();
     }
 }
