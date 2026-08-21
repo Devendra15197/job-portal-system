cd4858e00ca54cd6acd853251e483cb5ab7ca7d0
devendrapawar <devendrapawar666@gmail.com>
2026-08-21 18:42:45 +0530

    Updated Application Repository

--- services/job-portal-application-service/src/main/java/com/zosh/job/repository/ApplicationRepository.java
+++ services/job-portal-application-service/src/main/java/com/zosh/job/repository/ApplicationRepository.java
@@ -11,4 +11,6 @@ public interface ApplicationRepository extends JpaRepository<Application, Long>,
     List<Application> findByCandidateId(Long candidateId);
     List<Application> findByJobId(Long jobId);
     boolean existsByCandidateIdAndJobId(Long candidateId, Long jobId);
+
+
 }
