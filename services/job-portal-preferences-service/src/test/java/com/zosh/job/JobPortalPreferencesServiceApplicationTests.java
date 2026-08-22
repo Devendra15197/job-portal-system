0d8308f6d6706ca72cbe2223fca3e24c1145f7ca
devendrapawar <devendrapawar666@gmail.com>
2026-08-22 16:05:34 +0530

    Main class and test class

---
+++ services/job-portal-preferences-service/src/test/java/com/zosh/job/JobPortalPreferencesServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.zosh.job;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalPreferencesServiceApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
