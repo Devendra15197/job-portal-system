069a08e291ec0c91dfcceedc122613f77d3706ad
devendrapawar <devendrapawar666@gmail.com>
2026-08-26 17:00:54 +0530

    JwtUtil implemented for extracting claims

---
+++ cloud/job-portal-api-gateway/src/main/java/com/zosh/job/jwt/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.zosh.job.jwt;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "mySecretKeyForJwtTokenGenerationAndValidation12345";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String JWT_HEADER = "Authorization";
+}
