273187a847231a06f4fc2bf4731373dfd3a0502f
devendrapawar <devendrapawar666@gmail.com>
2026-08-31 23:11:02 +0530

    Resume Feign Client mapping corrected

--- services/job-portal-application-service/src/main/java/com/zosh/job/client/ResumeClient.java
+++ services/job-portal-application-service/src/main/java/com/zosh/job/client/ResumeClient.java
@@ -2,14 +2,13 @@ package com.zosh.job.client;
 
 import com.zosh.job.dto.ResumeResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "JOB-PORTAL-RESUME-SERVICE")
+@FeignClient(name = "job-portal-resume-service")
 public interface ResumeClient {
-    @GetMapping("/{resumeId}")
-    ResumeResponse getResumeById(@PathVariable Long resumeId,
+    @GetMapping("/api/resumes/{resumeId}")
+    ResumeResponse getResumeById(@PathVariable("resumeId") Long resumeId,
                                  @RequestHeader("X-User-Id") Long candidateId);
 }
