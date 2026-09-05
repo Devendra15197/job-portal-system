70fec584e51d8f59ae48702de9df61b498883a8d
devendrapawar <devendrapawar666@gmail.com>
2026-09-05 23:47:26 +0530

    Created Gemini AI properties

--- services/job-portal-ai-service/src/main/java/com/zosh/job/config/GeminiProperties.java
+++ services/job-portal-ai-service/src/main/java/com/zosh/job/config/GeminiProperties.java
@@ -1,13 +1,15 @@
 package com.zosh.job.config;
 
-import lombok.Builder;
 import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @Data
-@Builder
+@Component
+@ConfigurationProperties(prefix = "gemini.api")
 public class GeminiProperties {
     private String key;
     private String model;
-    private int maxOutputToken = 2048;
+    private int maxOutputTokens = 2048;
     private double temperature = 0.7;
 }
