bf5037d5808cdb004acbd25887c1ea52da36f764
devendrapawar <devendrapawar666@gmail.com>
2026-09-25 23:42:26 +0530

    Annotations added

--- services/job-portal-ai-service/src/main/java/com/zosh/job/payload/SalaryRangeResponse.java
+++ services/job-portal-ai-service/src/main/java/com/zosh/job/payload/SalaryRangeResponse.java
@@ -1,8 +1,10 @@
 package com.zosh.job.payload;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class SalaryRangeResponse {
     private String minSalary;
     private String maxSalary;
